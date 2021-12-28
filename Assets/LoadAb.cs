e934590e9835cafabc48a798766d5bb612e51580
何济东 <491799022@qq.com>
2021-12-28 16:01:10 +0800

    Init_2

---
+++ Assets/LoadAb.cs
@@ -0,0 +1,26 @@
+﻿/****************************************************
+    文件：LoadAb.cs
+	作者：Plane
+    邮箱: 1785275942@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+
+using System.IO;
+using UnityEngine;
+
+public class LoadAb : MonoBehaviour 
+{
+
+    private void Start()
+    {
+        InstanceBundle("cube", "Sphere");
+    }
+
+    void InstanceBundle(string bundleName,string assetName)
+    {
+        AssetBundle asset = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
+        var asset_obj = asset.LoadAsset<GameObject>(assetName);
+        Instantiate(asset_obj);
+    }
+}
---
+++ Assets/NavTest.cs
@@ -0,0 +1,66 @@
+﻿/****************************************************
+    文件：NavTest.cs
+	作者：Plane
+    邮箱: 1785275942@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour 
+{
+    private NavMeshAgent navMeshAgent;
+    public Transform Node1;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        Debug.Log(Mathf.Cos(90));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void TestNavMove()
+    {
+        Vector3 curPos = transform.position;
+        Vector3 dstPos = Node1.position;
+        Vector3 proPos;
+        bool checkDisable = false;
+        if (dstPos.y > curPos.y)
+        {
+            proPos = new Vector3(dstPos.x,curPos.y,curPos.z);
+        }
+        else
+        {
+            proPos = new Vector3(curPos.x, curPos.y, dstPos.z);
+        }
+
+        navMeshAgent.SetDestination(proPos);
+
+
+        StartCoroutine(LoopDetection(checkDisable, () => {
+            if (Vector3.Distance(transform.position, proPos) <= 0.15f)
+            {
+                checkDisable = true;
+                navMeshAgent.SetDestination(dstPos);
+            }
+        }));
+             
+    }
+
+    IEnumerator LoopDetection(bool disable,Action action)
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            action();
+            if (disable) break;
+        }
+    }
+}
